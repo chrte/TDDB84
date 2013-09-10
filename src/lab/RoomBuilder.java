c0d4965565ad0b7a7f94f9317a52b61bc3192186
hento581 <henrik.tosteberg@gmail.com>
2013-09-10 16:00:11 +0200

    La till interface for buildern

---
+++ src/lab/RoomBuilder.java
@@ -0,0 +1,14 @@
+package lab;
+
+public interface RoomBuilder {
+
+	 public void buildWalls();
+	 
+	  public void buildDoors();
+	 
+	  public void addSides();
+	 
+	  public Room getRoom();
+	
+	
+}
