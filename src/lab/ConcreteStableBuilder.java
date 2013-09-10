d3600c2df386172825e1d0652c247e1de1cecc6e
hento581 <henrik.tosteberg@gmail.com>
2013-09-10 16:29:31 +0200

    Skapade concreteklassen och gillar att commita saker, visst är det kul @chrte ?

---
+++ src/lab/ConcreteStableBuilder.java
@@ -0,0 +1,23 @@
+package lab;
+
+public class ConcreteStableBuilder extends StableBuilder {
+
+	@Override
+	public void buildRooms() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void buildDoors() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void buildWalls() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
