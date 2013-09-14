2b75c2f96248d38bc119384c9afeb91fb51fa9d0
chrte <christian@tennstedt.se>
2013-09-14 11:27:57 +0200

    Fourth and last task of lab3 is now finished. I was doing this while @hento581 was lost in the forest north of Finspång.

--- src/lab/Decorator.java
+++ src/lab/Decorator.java
@@ -1,10 +1,13 @@
 package lab;
 
+import java.awt.Color;
 import java.awt.Rectangle;
+import java.util.AbstractList;
+import java.util.Iterator;
 
 // YOUR CODE HERE
 // extends? implements?
-class Decorator {
+class Decorator implements Renderer {
 
 	private Renderer decorated;
 
@@ -19,6 +22,56 @@ class Decorator {
 		g.drawLine(r.x + 2, r.y - 2, r.x + r.width + 2, r.y - 2);
 		g.drawLine(r.x + r.width + 2, r.y - 2,
 				r.x + r.width + 2, r.y - 2 + r.height);
+	}
+
+	@Override
+	public void putBackground() {
+		decorated.putBackground();
+		
+	}
+
+	@Override
+	public void putBody(AbstractList<Rectangle> body) {
+		decorated.putBody(body);
+		
+		Rectangle r;		
+		Iterator<Rectangle> iterator = body.iterator();
+		while (iterator.hasNext()) {
+			r = iterator.next();
+			decorateRectangle(r);
+		}
+		
+	}
+
+	@Override
+	public void setSnakeColor(Color color) {
+		decorated.setSnakeColor(color);
+		
+	}
+
+	@Override
+	public void putBonus(AbstractList<Rectangle> bonus) {
+		decorated.putBonus(bonus);
+		
+		Rectangle r;		
+		Iterator<Rectangle> iterator = bonus.iterator();
+		while (iterator.hasNext()) {
+			r = iterator.next();
+			decorateRectangle(r);
+		}
+		
+	}
+
+	@Override
+	public void outside() {
+		decorated.outside();
+		
+	}
+
+	@Override
+	public void stop() {
+		decorated.stop();
+		
 	}
 
 	// YOUR CODE HERE
